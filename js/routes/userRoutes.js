ca04473151b3a6deb3d67dd440e7b549bd8c1161
ioanaalexia <popaalexia371@gmail.com>
2024-06-17 17:41:00 +0300

    user

--- js/routes/userRoutes.js
+++ js/routes/userRoutes.js
@@ -1,36 +1,54 @@
 const UserController = require('../controllers/UserController');
+const cookie=require('cookie')
+const AuthService = require('../services/AuthService');
 const userController = new UserController();
+const authService= new AuthService();
 
-// Functia de routing pentru utilizator
-function routeController(req, res, pathname, method) {
-    if (method === 'POST') {
-        if (pathname === '/signup') {
-            userController.newUser(req, res);
-        } else if (pathname === '/login') {
-            userController.loginUser(req, res);
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-        }
-    } else if (method === 'GET') {
-        if (pathname === '/logout') {
-            userController.logoutUser(req, res);
-        } else if (pathname === '/api/user-info') {
-            if (userData) {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(userData));
-            } else {
-                res.writeHead(401, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'User not authenticated' }));
-            }
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-        }
-    } else {
-        res.writeHead(405, { 'Content-Type': 'text/plain' });
-        res.end('Method Not Allowed');
+function userRoutes(req, res, pathname, method) {
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const token = cookies.authToken;
+    const userData = token ? authService.decodeToken({ headers: { cookie: req.headers.cookie } }) : null;
+
+    switch(method) {
+        case 'GET':
+            handleGetRequest(req, res, pathname, userData);
+            break;
+        case 'POST':
+            handlePostRequest(req, res, pathname, userData);
+            break;
+        default:
+            res.writeHead(405, { 'Content-Type': 'text/plain' });
+            res.end('Method Not Allowed');
+            break;
     }
 }
 
-module.exports = routeController;
+function handleGetRequest(req, res, pathname, userData) {
+    if (pathname === '/logout') {
+        userController.logoutUser(req, res);
+    } else if (pathname === '/api/user-info') {
+        if (userData) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(userData));
+        } else {
+            res.writeHead(401, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'User not authenticated' }));
+        }
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+}
+
+function handlePostRequest(req, res, pathname, userData) {
+    if (pathname === '/login') {
+        userController.loginUser(req, res);
+    } else if (pathname === '/signup') {
+        userController.newUser(req, res);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+}
+
+module.exports = userRoutes;
--- server.js
+++ server.js
@@ -2,23 +2,12 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
-<<<<<<< Updated upstream
 const cookie = require('cookie');
 const routeController = require('./js/routes/userRoutes');
-=======
-const cookie=require('cookie')
-const { StringDecoder } = require('string_decoder');
-const UserController = require('./js/controllers/UserController');
-const AuthService = require('./js/services/AuthService');
-const userController = new UserController();
-const authService= new AuthService();
->>>>>>> Stashed changes
 
 const server = http.createServer(async (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
-    const cookies = cookie.parse(req.headers.cookie || '');
-<<<<<<< Updated upstream
     const method = req.method;
 
     if (pathname.startsWith('/api/') || pathname === '/login' || pathname === '/signup' || pathname === '/logout') {
@@ -32,64 +21,6 @@ const server = http.createServer(async (req, res) => {
     } else if (pathname === '/') {
         fs.readFile('./html/index.html', 'utf8', (err, data) => {
             if (err) {
-=======
-    const token = cookies.authToken;
-    const userData = token ? authService.decodeToken({ headers: { cookie: req.headers.cookie } }) : null;
-    
-    if (req.url === '/api/user-info' && req.method === 'GET') {
-        if (userData) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(userData));
-        } else {
-            res.writeHead(401, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User not authenticated' }));
-        }
-        return;
-    }
-
-    if (req.method === 'GET') {
-        
-       
-
-        if(pathname==='/html/start.html'){
-            console.log("sunt pe start")
-            await userController.startupUser(req, res);    
-        }
-        if (pathname.match(/\.(html|css|js|png|jpg|jpeg|svg)$/)) {
-            serveStaticFile(res, pathname);
-           
-            return;
-        }
-
-        // Servește pagina principală
-        if (pathname === '/') {
-            fs.readFile('./html/index.html', 'utf8', (err, data) => {
-                if (err) {
-                    res.writeHead(404);
-                    res.end("404 Not Found");
-                } else {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(data);
-                }
-            });
-        }else if(pathname==='/logout'){
-            await userController.logoutUser(req, res); 
-        }
-        else {
-            res.writeHead(404);
-            res.end("404 Not Found");
-        }
-    } else if (req.method === 'POST') {
-        console.log(pathname)
-
-            
-            if (pathname === '/login') {
-                await userController.loginUser(req, res);    
-            } else if(pathname === '/signup'){
-                await userController.newUser(req, res);
-            }
-            else {
->>>>>>> Stashed changes
                 res.writeHead(404);
                 res.end("404 Not Found");
             } else {
